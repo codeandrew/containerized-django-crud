061a1f499e4499d1850a5150a67d158c8d2837d8
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2019-05-06 10:35:27 +0800

    Create Programmers, Paradigm apis, and made them relational

--- django/languages/serializers.py
+++ django/languages/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from .models import Language
+from .models import Language, Paradigm, Programmer
 
 class LanguageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Language
         fields = ( 'id', 'name', 'paradigm', 'url')
+
+class ParadigmSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Paradigm
+        fields = ('id', 'name', 'url')
+
+class ProgrammerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Programmer
+        fields = ('id', 'name', 'languages', 'url')
